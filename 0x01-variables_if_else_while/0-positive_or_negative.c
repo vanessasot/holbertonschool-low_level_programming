56a73a3afb5a47ce85e7d7af74412b910c894e42
vanessasot <vannexxaa@gmail.com>
2020-06-11 21:59:37 +0000

    Conditional if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 
 	if (n < 0)
 	{
-		printf("%d is negative\n",n);
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is posititive\n",n);
+		printf("%d is posititive\n", n);
 	}
 	return (0);
 }
