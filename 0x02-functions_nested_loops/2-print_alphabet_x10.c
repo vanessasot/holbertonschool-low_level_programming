40d8d3c9a936e59428f5042c65f767a85eb32c53
vanessasot <vannexxaa@gmail.com>
2020-06-16 05:40:39 +0000

    Function alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,23 +4,20 @@
 #include "holberton.h"
 
 /**
- * print_alphabet - Prints the alphabet in lowercase.
- *
- * _putchar - Print the characters.
+ * print_alphabet_x10 - Prints the alphabet in lowercase.
  *
  * Return: 0 success
  */
 void print_alphabet_x10(void)
 {
-	int school[26] = {97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122};
 	int i;
 	int n;
 
 	for (n = 0; n < 10; n++)
 	{
-		for (i = 0; i <= 26 - 1; i++)
+		for (i = 97; i <= 122 - 1; i++)
 		{
-			_putchar(school[i]);
+			_putchar(i);
 		}
 		_putchar('\n');
 	}
