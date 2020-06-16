837885bedab349151b09f35779ff8c7bd88fb6ed
vanessasot <vannexxaa@gmail.com>
2020-06-16 21:08:04 +0000

    Add prototype

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,13 +1,20 @@
 #ifndef HOLBERTON
 #define HOLBERTON
 
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Prototypes */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
-int _abs(int);
+int _abs(int n);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
@@ -15,4 +22,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif /* HOLBERTON */
