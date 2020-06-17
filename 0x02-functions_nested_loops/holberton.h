52d1ac37ec57c93cd1350cc324cff00ef26823d9
vanessasot <vannexxaa@gmail.com>
2020-06-17 23:06:14 +0000

    Add prototype

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -19,7 +19,7 @@ int _abs(int n);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
-int add(int, int);
+int add(int n1, int n2);
 void print_to_98(int n);
 void print_times_table(int n);
 
