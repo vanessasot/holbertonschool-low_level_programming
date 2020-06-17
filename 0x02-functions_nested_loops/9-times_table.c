006dfbcd5ef7369ea4156cab2c57f1c6b762c46f
vanessasot <vannexxaa@gmail.com>
2020-06-17 22:50:00 +0000

    Function that prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * times_table - Prints the 9 times table.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int x;
+	int y;
+
+	x = 0;
+	i = 0;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			x = i * j;
+			if (x < 10)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				y = x / 10;
+				_putchar(y + '0');
+			}
+			y = x % 10;
+			_putchar (y + '0');
+			if (j == 9)
+			{
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+			}
+			j++;
+		}
+		i++;
+	}
+}
