ccdb7762cd9d60cb0a471421bb5c8ae2ec972647
vanessasot <vannexxaa@gmail.com>
2020-06-18 17:30:05 +0000

    Header file

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /* HOLBERTON */
