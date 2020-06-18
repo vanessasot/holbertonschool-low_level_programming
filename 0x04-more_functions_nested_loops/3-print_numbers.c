b0a5c4c5dcf95e92a0cd69b613c699b00b82c12b
vanessasot <vannexxaa@gmail.com>
2020-06-18 18:58:41 +0000

    Function tha prints the numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9.
+ *
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	 _putchar('\n');
+}
