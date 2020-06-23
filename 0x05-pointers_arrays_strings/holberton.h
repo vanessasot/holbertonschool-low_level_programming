6881dcdf7df5365cfeabef1948feb56473e26fe8
vanessasot <vannexxaa@gmail.com>
2020-06-23 03:26:22 +0000

    Add prototype dereferencing

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* Prototypes */
+
+void reset_to_98(int *n);
+
+#endif /* HOLBERTON */
