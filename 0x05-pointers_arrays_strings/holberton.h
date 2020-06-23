e4e7c32f19c8e56db4fe9d8f6d44ebcd5aeba43b
vanessasot <vannexxaa@gmail.com>
2020-06-23 21:09:28 +0000

    Add prototype putchar

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -6,10 +6,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 /* Prototypes */
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
 
 #endif /* HOLBERTON */
