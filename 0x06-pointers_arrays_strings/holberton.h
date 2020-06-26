8632f2e048f784672769a9b1b174ae212ef59c69
vanessasot <vannexxaa@gmail.com>
2020-06-26 00:59:10 +0000

    Add prototype strncat

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+char *_strncat(char *dest, char *src, int n);
+
+#endif /* HOLBERTON */
