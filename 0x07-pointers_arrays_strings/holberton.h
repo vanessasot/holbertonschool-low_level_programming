4662a9e23e210b700597884245c3294ea2ac9a61
vanessasot <vannexxaa@gmail.com>
2020-06-29 02:47:30 +0000

    Create header file

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* HOLBERTON */
