97c806d602cc9ea4416f1d2497eee041427c25c7
vanessasot <vannexxaa@gmail.com>
2020-07-01 19:35:38 +0000

    Function that prints a string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Prints a string.
+ * @s: Pointer to character.
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar (*s);
+	s++;
+
+	if (*s != '\0')
+	{
+		_puts_recursion(s);
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
