4eb893094104640d9ea3ea78ebf9352b07dfa30a
vanessasot <vannexxaa@gmail.com>
2020-07-04 16:27:23 +0000

    Add prototypes sqrt_recursion, sqrt2

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -16,5 +16,7 @@ void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt2(int a, int b);
 
 #endif /* HOLBERTON */
