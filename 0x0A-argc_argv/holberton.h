c3e18dbb966c1bd2e4f189d055dc0388554bc1b0
vanessasot <vannexxaa@gmail.com>
2020-07-05 00:29:01 +0000

    Create header file

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+
+#endif /* HOLBERTON */
