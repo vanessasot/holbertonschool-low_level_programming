107b8cac6e0b0d0ac5d3d0a3fd9190e0eca4377f
vanessasot <vannexxaa@gmail.com>
2020-07-06 02:57:17 +0000

    Program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * main - Adds positive numbers.
+ * @argc: Number the parameters.
+ * @argv: Element.
+ *
+ * Return: 0 success
+ */
+
+int main(int argc, char *argv[])
+{
+	int suma;
+	int i;
+	int j;
+
+	suma = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; *argv[i] != '\0'; j++)
+		{
+			if (*(argv[i] + j) >= 48 && *(argv[i] + j) <= 57)
+			{
+				suma = suma + atoi(argv[i] + j);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", suma);
+	return (0);
+}
+
