35f34b00186f2ccfd2eec152377f767e68ed0c80
vanessasot <vannexxaa@gmail.com>
2020-07-07 21:21:38 +0000

    Create header file

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c);
+
+#endif /* HOLBERTON */
