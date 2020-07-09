35c8002a1fe05023c9e520076d875cd715d0094f
vanessasot <vannexxaa@gmail.com>
2020-07-09 18:04:50 +0000

    Add prototype malloc_checked

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,16 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+
+#endif /* HOLBERTON */
