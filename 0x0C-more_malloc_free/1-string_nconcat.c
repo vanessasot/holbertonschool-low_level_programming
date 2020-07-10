047fb081b444e02d3a2c24267b9be63422fba8bf
vanessasot <vannexxaa@gmail.com>
2020-07-10 02:42:05 +0000

    Function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,42 +11,36 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *cadena;
-	unsigned int i, a, b;
+	unsigned int a, b, c, d;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 	for (a = 0; s1[a] != '\0'; a++)
+	{
+	}
 	for (b = 0; s2[b] != '\0'; b++)
-	cadena = malloc((a + n + 1) * sizeof(char));
+	{
+	}
+	if (n >= b)
+		n = b;
+	cadena = malloc(((a + n) + 1) * sizeof(char));
 	if (cadena == NULL)
 		return (NULL);
-	if (n >= b)
+	c = 0;
+	while (c < a)
 	{
-		for (i = 0; *s2 != '\0'; i++)
-		{
-			cadena[i] = *s2;
-			s2++;
-		}
-		cadena[i] = '\0';
-		return (cadena);
+		cadena[c] = s1[c];
+		c++;
 	}
-	i = 0;
-	while (*s1 != '\0')
+	d = 0;
+	while (d < n)
 	{
-		cadena[i] = *s1;
-		s1++;
-		i++;
+		cadena[c + d] = s2[d];
+		d++;
 	}
-		while (*s2 != '\0')
-		{	
-			cadena[i] = *s2;
-			s2++;
-			i++;
-			a++;
-		}
-	cadena[i] = '\0';
-	return (cadena);
+	cadena[c + d] = '\0';
+	return (cadena);	
 }
 
