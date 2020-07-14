f4e668f3d99d0a5d2a778dcc2ba97b587f8e563c
vanessasot <vannexxaa@gmail.com>
2020-07-14 07:02:15 +0000

    Change

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,8 +28,8 @@ typedef struct dog
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG */
 
