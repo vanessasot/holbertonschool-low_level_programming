a32d24763cdb9f3f0e64ede6e00fcc4c56c32bb2
vanessasot <vannexxaa@gmail.com>
2020-07-14 04:35:15 +0000

    Add prototype free_dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -28,5 +28,7 @@ typedef struct dog
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_dog(dog_t *d);
 
 #endif /* DOG */
+
