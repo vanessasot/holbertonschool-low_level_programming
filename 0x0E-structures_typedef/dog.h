5fb8b071acfaa81cd81254c137b0f0b925a589a1
vanessasot <vannexxaa@gmail.com>
2020-07-14 01:10:39 +0000

    Create a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Data of a dog.
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Composition of the structure.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG */
