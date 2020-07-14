2a6c36f1932b3a0d2045ff2da8c5afca1b961020
vanessasot <vannexxaa@gmail.com>
2020-07-14 06:30:55 +0000

    Function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,41 @@
+#include "dog.h"
+
+/**
+ * new_dog - Function that creates.
+ * @name: Pointer to array.
+ * @age: Number of age.
+ * @owner: Pointer to array.
+ *
+ * Return: Pointer to struct.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	int i, j, a, b;
+
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+	for (j = 0; owner[j] != '\0'; j++)
+
+	d->name = malloc(i + 1 * sizeof(char *));
+	if (d->name == NULL)
+		return (NULL);
+	d->owner = malloc(j + 1 * sizeof(char *));
+	if (d->owner == NULL)
+		return (NULL);
+
+	d->age = age;
+
+	for (a = 0; a < (i + 1); a++)
+	{
+		d->name[a] = name[a];
+	}
+	for (b = 0; b < (j + 1); b++)
+	{
+		d->owner[b] = owner[b];
+	}
+	return (d);
+}
