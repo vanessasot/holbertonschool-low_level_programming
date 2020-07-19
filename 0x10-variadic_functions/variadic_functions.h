5047964f625a8e7ea8dafb5bb4cf2069d9551cb0
vanessasot <vannexxaa@gmail.com>
2020-07-19 01:08:27 +0000

    Header file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,16 @@
+#ifndef VARIADIC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif /* VARIADIC_FUNCTIONS */
