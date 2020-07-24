6067e4c1d5c5063ec8c1daab92d7045e5d2c493d
vanessasot <vannexxaa@gmail.com>
2020-07-24 05:50:27 +0000

    Add prototype print linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,6 +28,7 @@ typedef struct list_s
 /* Prototypes */
 
 int _putchar(char c);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 void first(void) __attribute__ ((constructor));
 
