777a55266d1db9428351e2133878fe234f1a4ea6
vanessasot <vannexxaa@gmail.com>
2020-07-29 05:03:08 +0000

    Function that deletes the node at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(aux);
 		return (1);
 	}
-	while (counter < index && aux != NULL)
+	while (counter < index - 1 && aux != NULL)
 	{
 		aux = aux->next;
 		counter++;
