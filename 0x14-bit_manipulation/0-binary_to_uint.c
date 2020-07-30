eb4260a0239b0e71d0023c4c9b5bd24e04e3524f
vanessasot <vannexxaa@gmail.com>
2020-07-30 22:10:36 +0000

    Function that converts a binary numbers to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Binary number
+ *
+ * Return: Address of the new node.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0;
+	int multiplicador = 1;
+	int i;
+	char caracterActual;
+
+	if (b == NULL)
+		return (0);
+	for (i = strlen(b) - 1; i >= 0; i--)
+	{
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		caracterActual = b[i];
+		if (caracterActual == '1')
+			dec += multiplicador;
+		multiplicador = multiplicador * 2;
+	}
+	return (dec);
+}
