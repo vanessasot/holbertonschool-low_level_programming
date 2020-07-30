dd5e3a183e7a15379192b2c3cfcd37f4031eac13
vanessasot <vannexxaa@gmail.com>
2020-07-30 22:09:24 +0000

    Create header file

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* Prototypes */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* HOLBERTON */
