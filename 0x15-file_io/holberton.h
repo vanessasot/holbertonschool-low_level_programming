9ad1b5aa117d22787652e2a8d4e25415f208156e
vanessasot <vannexxaa@gmail.com>
2020-08-05 01:57:55 +0000

    Create header file

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Standard Libraries*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/* Prototypes */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* HOLBERTON */
