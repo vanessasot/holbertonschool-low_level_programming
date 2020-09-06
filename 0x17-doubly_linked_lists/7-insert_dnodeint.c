aed3a414de507ead62336910224cbd65761a165e
Vanessa <vannexxaa@gmail.com>
2020-09-06 23:55:48 +0000

    Write a function that inserts a new node at a given position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,7 +12,8 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *newNode = NULL;
-	dlistint_t **aux = &(*h);
+	dlistint_t *aux = *h;
+	dlistint_t *sig;
 	unsigned int counter = 0;
 
 	newNode = malloc(sizeof(dlistint_t *));
@@ -20,16 +21,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (newNode == NULL)
 		return (NULL);
 	newNode->n = n;
-	while (counter < idx && *aux != NULL)
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (idx == 1)
 	{
-		aux = &(*aux)->next;
-		counter++;
-	}
-	if (idx == counter)
-	{
-		newNode->next = *aux;
-		*aux = newNode;
+		aux->next = newNode;
+		sig = aux->next;
+		sig->prev = newNode;
+		newNode->next = sig;
+		sig->prev = newNode;
+		newNode->prev = aux;
 		return (newNode);
 	}
-	return (NULL);
+	while (counter != idx - 1 && aux != NULL)
+	{
+		aux = aux->next;
+		if (aux == NULL)
+			return (NULL);
+		sig = aux->next;
+		counter++;
+	}
+	if (counter < idx - 1)
+		return (NULL);
+	if (sig == NULL)
+		return (add_dnodeint_end(h, n));
+	aux->next = newNode;
+	newNode->next = sig;
+	newNode->prev = aux;
+	if (sig != NULL)
+		sig->prev = newNode;
+	return (newNode);
 }
