9c1e830dbbf00c0f1fefe67a7ba92e001150f355
Vanessa <vannexxaa@gmail.com>
2020-10-02 22:54:01 +0000

    Header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -15,12 +15,11 @@
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
-
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -31,11 +30,10 @@ typedef struct hash_node_s
  * Each cell of this array is a pointer to the first node of a linked list,
  * because we want our HashTable to use a Chaining collision handling
  */
-
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 /* Prototypes */
