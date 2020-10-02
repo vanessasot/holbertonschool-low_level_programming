5da5acca2f7a0e05dfe5618b122eb38abff5b338
Vanessa <vannexxaa@gmail.com>
2020-10-02 23:29:09 +0000

    Function that create the index of the key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - Create index of the key.
+ * @key: Key of the element of the table.
+ * @size: Size of the hash table.
+ *
+ * Return: Index for the key.
+ */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash_t;
